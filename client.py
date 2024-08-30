a320b74a754016406f607b86122c8ccc7ee934ac
ryansereno <ryansereno@protonmail.com>
2024-08-30 16:10:48 -0400

    updated client script to allow continuous input

--- client.py
+++ client.py
@@ -1,15 +1,24 @@
 import serial
 import time
+import getch
 
 ser = serial.Serial("/dev/tty.usbmodem101", 9600, timeout=1)
 time.sleep(2)
 
+print("Start typing (press 'Esc' to quit):")
+
 while True:
-    char = input("Enter a character to send (or 'q' to quit): ")
-    if char == "q":
+    char = getch.getch()
+
+    if ord(char) == 27:
         break
+
     ser.write(char.encode())
+    print(char, end="", flush=True)
+
     received = ser.read().decode()
-    print(f"Echoed back: {received}")
+    if received:
+        print(f"\nEchoed back: {received}")
 
 ser.close()
+print("\nConnection closed.")
