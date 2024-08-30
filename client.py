4d78963bce26bcf7a5d48832e3de7f1b4372ac91
ryansereno <ryansereno@protonmail.com>
2024-08-29 22:04:59 -0400

    added client script

---
+++ client.py
@@ -0,0 +1,15 @@
+import serial
+import time
+
+ser = serial.Serial("COM3", 9600, timeout=1)
+time.sleep(2)
+
+while True:
+    char = input("Enter a character to send (or 'q' to quit): ")
+    if char == "q":
+        break
+    ser.write(char.encode())
+    received = ser.read().decode()
+    print(f"Echoed back: {received}")
+
+ser.close()
